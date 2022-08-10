be3f20390c6d99d6a81ad2ed1549a67bd7aa0354
Mickey DeLorenzo <mdelorenzo@byrdalley.com>
2022-08-10 16:35:25 -0400

    text

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => {
       <section className="py-5 text-center container">
         <div className="row py-lg-5">
           <div className="col-lg-6 col-md-8 mx-auto">
-            <h1 className="fw-light">Hello world !</h1>
+            <h1 className="fw-light">Hello world!</h1>
             <p className="lead text-muted">Company Name</p>
             <StaticImage
               src="../images/gatsby-astronaut.png"
