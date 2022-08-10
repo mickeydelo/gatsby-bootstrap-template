3bbd8ddf76c7c2f12796528d0956f16a60868e60
Mickey DeLorenzo <mdelorenzo@byrdalley.com>
2022-08-10 19:56:09 -0400

    Responsive Component Styles

--- src/components/modules/image-text.js
+++ src/components/modules/image-text.js
@@ -4,14 +4,14 @@ function ImageText (props) {
   return (
     <div className="container my-5 py-5">
       <div class="row featurette">
-        <div class="col-md-7 order-md-2">
+        <div class="col-md-7 order-md-2 center-responsive">
           <h2 class="featurette-heading">{props.header} <span class="text-muted">{props.subhead}</span></h2>
           <p class="lead">{props.copy}</p>
           <button className={`${ props.btnStyle }`}>
             {props.btnName}
           </button>
         </div>
-        <div className={`col-md-5 ${ props.imgPos }`}>
+        <div className={`col-md-5 center-responsive ${ props.imgPos }`}>
           <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src={props.image} alt="icon" />
         </div>
       </div>
